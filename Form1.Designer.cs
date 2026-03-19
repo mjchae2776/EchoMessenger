8e3b26978166136e04db681817d27e7c4b86dc4f
민정 <mjchae2776@naver.com>
2026-03-19 17:00:53 +0900

    과제1 코딩 미완성 UI 구성 후 전송 기능 구현 완료

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            lbl = new Label();
+            lsbMsg = new ListBox();
+            txtMsg = new TextBox();
+            btnSend = new Button();
+            SuspendLayout();
+            // 
+            // lbl
+            // 
+            lbl.AutoSize = true;
+            lbl.BackColor = Color.Cornsilk;
+            lbl.Font = new Font("Georgia", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbl.ForeColor = SystemColors.HotTrack;
+            lbl.Location = new Point(33, 34);
+            lbl.Name = "lbl";
+            lbl.Size = new Size(371, 56);
+            lbl.TabIndex = 0;
+            lbl.Text = "Echo Messenger";
+            // 
+            // lsbMsg
+            // 
+            lsbMsg.FormattingEnabled = true;
+            lsbMsg.Location = new Point(142, 132);
+            lsbMsg.Name = "lsbMsg";
+            lsbMsg.Size = new Size(403, 199);
+            lsbMsg.TabIndex = 1;
+            lsbMsg.SelectedIndexChanged += lsbMsg_SelectedIndexChanged;
+            // 
+            // txtMsg
+            // 
+            txtMsg.Location = new Point(142, 362);
+            txtMsg.Name = "txtMsg";
+            txtMsg.Size = new Size(403, 23);
+            txtMsg.TabIndex = 2;
+            txtMsg.TextChanged += txtMsg_TextChanged;
+            // 
+            // btnSend
+            // 
+            btnSend.BackColor = Color.NavajoWhite;
+            btnSend.Location = new Point(563, 361);
+            btnSend.Name = "btnSend";
+            btnSend.Size = new Size(75, 23);
+            btnSend.TabIndex = 3;
+            btnSend.Text = "전송";
+            btnSend.UseVisualStyleBackColor = false;
+            btnSend.Click += btnSend_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Cornsilk;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnSend);
+            Controls.Add(txtMsg);
+            Controls.Add(lsbMsg);
+            Controls.Add(lbl);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lbl;
+        private ListBox lsbMsg;
+        private TextBox txtMsg;
+        private Button btnSend;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 namespace EchoMessenger
 {
     public partial class Form1 : Form
@@ -5,6 +7,23 @@ namespace EchoMessenger
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void txtMsg_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string typed_msg;
+            typed_msg = txtMsg.Text;
+            lsbMsg.Items.Add(typed_msg);
+        }
+
+        private void lsbMsg_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
